e73538a2f10c8eddd911d223cdcaf4729273f556
haider-codes23 <haide.codes23@gmail.com>
2024-07-30 20:30:05 +0500

    Moved Genre Model to a seperate module

---
+++ vidly/models/genresModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+// define a schema for genre 
+const genreSchema = new mongoose.Schema({ 
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+
+});
+
+// create a model for this genres
+const Genre = mongoose.model('genre', genreSchema);
+
+module.exports = Genre;
